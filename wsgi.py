88fd889dfc4dc907e97e87ca0cf95e7773d80591
salmanml277-stack <salmanml277@gmail.com>
2026-05-18 20:18:50 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add your project directory to the path
-path = '/home/salmanml277-stack/himasha-shavindi'
-if path not in sys.path:
-    sys.path.append(path)
-
-from app import app as application
